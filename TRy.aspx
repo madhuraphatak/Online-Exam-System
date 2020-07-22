f645ae41e10d3cbcc3e3fcbdcbe0414d4b1c6969
madhuraphatak <47379728+madhuraphatak@users.noreply.github.com>
2020-07-22 05:48:06 -0200

    Add files via upload

---
+++ TRy.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TRy.aspx.cs" Inherits="TRy" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+          <h2>Online Examination</h2>  
+           <hr />
+            Welcome <asp:Label ID="Label10" runat="server"></asp:Label>
+            <br />
+            <input id="Id" name="Id" type="hidden" value="1" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>&ensp;
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="a"/>
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label><br />
+            <asp:RadioButton ID="RadioButton2" runat="server" GroupName="a" />
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label><br />
+            <asp:RadioButton ID="RadioButton3" runat="server" GroupName="a" />
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label><br />
+            <asp:RadioButton ID="RadioButton4" runat="server" GroupName="a" />
+            <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label><br />
+            <br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Back" BorderStyle="Ridge" Font-Size="Small" ForeColor="#660066" OnClick="Back" />
+            &emsp;&emsp;&emsp;&emsp;
+            <asp:Button ID="Button3" runat="server" Text="Save" BorderStyle="Ridge" Font-Size="Small" ForeColor="#660066" OnClick="Save" />
+            &emsp;&emsp;&emsp;&emsp;
+            <asp:Button ID="Button2" runat="server" Text="Next" BorderStyle="Ridge" Font-Size="Small" ForeColor="#660066" OnClick="Next" />
+            
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ TRy.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class TRy : System.Web.UI.Page
+{
+    public static int i = 0;
+    SqlConnection scon;
+    SqlDataAdapter sda;
+    DataSet ds;
+    SqlCommand scmd;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userid"] != null)
+        {
+            string uid = Convert.ToString(Session["userid"]);
+            Label10.Text = uid;
+            
+        }
+        else
+        {
+            Response.Redirect("Failure.aspx");
+        }
+
+        scon = new SqlConnection("server=DESKTOP-E8Q152A\\SQLEXPRESS;uid=sa;pwd=microsoft;database=onlineexam");
+        sda = new SqlDataAdapter("select * from table_que;", scon);
+        ds = new DataSet();
+        sda.Fill(ds, "acc");
+        int cnt = ds.Tables["acc"].Rows.Count;
+        if (!IsPostBack)
+        {
+
+            //i = 0;
+            Label1.Text = ds.Tables["acc"].Rows[0]["qid"] + ")";
+            Label2.Text = Convert.ToString(ds.Tables["acc"].Rows[0]["que"]);
+            Label3.Text = Convert.ToString(ds.Tables["acc"].Rows[0]["op1"]);
+            Label4.Text = Convert.ToString(ds.Tables["acc"].Rows[0]["op2"]);
+            Label5.Text = Convert.ToString(ds.Tables["acc"].Rows[0]["op3"]);
+            Label6.Text = Convert.ToString(ds.Tables["acc"].Rows[0]["op4"]);
+
+        }
+    }
+
+    protected void Next(object sender, EventArgs e)
+    {
+        int cnt = ds.Tables["acc"].Rows.Count;
+        i++;
+        if (i < cnt)
+        {
+
+            Label1.Text = ds.Tables["acc"].Rows[i]["qid"] + ")";
+            Label2.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["que"]);
+            Label3.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op1"]);
+            Label4.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op2"]);
+            Label5.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op3"]);
+            Label6.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op4"]);
+
+        }
+        else
+        {
+            i = cnt;
+        }
+    }
+    protected void Back(object sender, EventArgs e)
+    {
+        i--;
+        int cnt = ds.Tables["acc"].Rows.Count;
+        if (i >= 0)
+        {
+
+            Label1.Text = ds.Tables["acc"].Rows[i]["qid"] + ")";
+            Label2.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["que"]);
+            Label3.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op1"]);
+            Label4.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op2"]);
+            Label5.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op3"]);
+            Label6.Text = Convert.ToString(ds.Tables["acc"].Rows[i]["op4"]);
+        }
+        else
+        {
+            i = 0;
+        }
+
+
+    }
+    protected void Save(object sender, EventArgs e)
+    {
+        
+
+    }   
+
+
+
+
+}
